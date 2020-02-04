c8e85060d8ce8456050a7cef9df42a793dff085d
Deondrede <38331348+Deondrede@users.noreply.github.com>
2020-02-04 12:55:09 -0500

    Add files via upload

---
+++ Project 1/animal.hpp
@@ -0,0 +1,23 @@
+/*Author: Deondre De Freitas
+Course: CSCI 235
+Creation: 02/04/20
+Header file for the Animal class*/
+
+#include <string>;
+using namespace std;
+
+class Animal {
+public:
+	Animal();
+	Animal(string name, bool domestic = false, bool predator = false);
+	string getName() const;
+	bool isDomestic() const;
+ 	bool isPredator() const;
+ 	void setName(string name);
+ 	void setDomestic();
+ 	void setPredator();
+ private:
+ 	string name_;
+ 	bool domestic_;
+ 	bool predator_;
+}
