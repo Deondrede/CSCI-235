791cea13b2b6659e7cb371335f0ebfbf919a31ee
Deondrede <38331348+Deondrede@users.noreply.github.com>
2020-02-04 12:54:54 -0500

    Create animal.cpp

---
+++ Project 1/animal.cpp
@@ -0,0 +1,48 @@
+/*Author: Deondre De Freitas
+Course: CSCI 235
+Creation: 02/04/20
+Implementation for the Animal class*/
+
+#include <"animal.hpp">;
+#include<string>;
+using namespace std;
+
+Animal::Animal(string name, bool domestic = false, bool predator = false){
+	name_ = name;
+	domestic_ = domestic;
+	predator_ = predator;
+}
+
+string Animal::getName() const{
+	return name_;
+}
+
+bool Animal::isDomestic() const{
+	return domestic_;
+}
+
+bool Animal::isPredator() const{
+	return predator_;
+}
+
+void Animal::setName(string name) {
+	name_ = name;
+}
+
+void Animal::setDomestic(){
+	if (domestic_ == false){
+		domestic_ = true;
+	}
+	else if (domestic_ == true){
+		domestic_ = false;
+	}
+}
+
+void Animal::setPredator(){
+	if (predator_ == false){
+		predator_ = true;
+	}
+	else if (predator_ == true){
+		predator_ = false;
+	}
+}
